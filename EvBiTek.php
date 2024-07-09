183f50f4b99f4601c400e8745548a226b1d017a6
EvgenProgromist <101187085+EvgenProgromist@users.noreply.github.com>
2024-07-09 10:40:54 +0300

    Первый пуш
    Весь код изменил в функции, для упрощения внешнего вида картинки, а также для того, чтобы в будущем можно было просто  добавить HTML, CSS и Java Script код.

--- EvBiTek.php
+++ EvBiTek.php
@@ -2,13 +2,57 @@
 
 class admin
 {
-    public $Govno = "govno";
+    public $Govno = "Hello world PHP!!";
 
-    function print_govno(): void
+    function print_Hi(): void
     {
-        echo "Govno";
+        echo "Hello world PHP!!";
     }
-
 }
 
+function logic_Operand() : void
+{
+    // Оператор "&&" имеет больший приоритет, чем "and"
 
+    // Результат выражения (true && false) будет присвоен переменной $bar1
+    // Действие приоритета: $bar1 = (true && false)
+    $bar1 = true && false;
+
+    // Сначала переменной присваивается значение true, а затем вычисляется второй операнд
+    // Действие приоритета: ($bar2 = true) and false
+    $bar2 = true and false;
+
+    var_dump($bar1, $bar2);
+
+    // ($bar3 = 9) and 3
+    $bar3 = 9 and 3;
+
+    echo "<br>$bar3";   // => 9
+    echo "<br>";
+
+    if ($bar1 OR $bar2 OR $bar3) {
+        echo "<br>";
+        echo "Govno";
+        echo "<br>";
+        echo "$bar1";
+        echo "<br>";
+        echo "$bar2";
+        echo "<br>";
+        echo "$bar3";
+    }
+}
+
+function logic_Operand2() : void
+{
+    echo "<br>";
+    $a = 15;
+    $b = 7;
+    $c = $a / $b;
+    $c1 = (int) ($a / $b);
+    echo "c1 = $c1";
+    echo "<br>";
+    echo "c = $c";
+
+    echo "<br>";
+
+}
--- phpinfo.php
+++ phpinfo.php
@@ -2,47 +2,14 @@
 
 require_once "EvBiTek.php";
 
-// Оператор "&&" имеет больший приоритет, чем "and"
+echo logic_Operand();
 
-// Результат выражения (true && false) будет присвоен переменной $bar1
-// Действие приоритета: $bar1 = (true && false)
-$bar1 = true && false;
+echo logic_Operand2();
 
-
-// Сначала переменной присваивается значение true, а затем вычисляется второй операнд
-// Действие приоритета: ($bar2 = true) and false
-$bar2 = true and false;
-
-var_dump($bar1, $bar2);
-
-// ($bar3 = 9) and 3
-$bar3 = 9 and 3;
-
-echo "<br>$bar3";   // => 9
-echo "<br>";
-
-if ($bar1 OR $bar2 OR $bar3) {
-    echo "<br>";
-    echo "Govno";
-    echo "<br>";
-    echo "$bar1";
-    echo "<br>";
-    echo "$bar2";
-    echo "<br>";
-    echo "$bar3";
-}
-
-echo "<br>";
-$a = 15;
-$b = 7;
-$c = $a / $b;
-$c1 = (int) ($a / $b);
-echo "c1 = $c1";
-echo "<br>";
-echo "c = $c";
+$admini_object = new admin;
 
 echo "<br>";
 
-print_govno();
+echo $admini_object->print_Hi();
 
 ?>
